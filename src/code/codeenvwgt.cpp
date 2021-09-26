ee9f4ba5f11cc5e50bdeaa12e3b189fd865bb832
MeGexik85 <sem0156@mail.ru>
2021-09-26 14:22:12 +0300

    featch(codeEnvWgt): make user interface

--- src/code/codeenvwgt.cpp
+++ src/code/codeenvwgt.cpp
@@ -2,12 +2,75 @@
 
 CodeEnvWgt::CodeEnvWgt(QWidget *parent) : QWidget(parent)
 {
+    QHBoxLayout *layout = new QHBoxLayout();
 
-    qDebug("CodeEnvWgt: success constructor");
+    pTranslateBtn = new QPushButton("-->", this);
+    connect(pTranslateBtn, SIGNAL(clicked()), this, SLOT(translate()));
+
+    layout->addLayout(getSrcTextLayout());
+    layout->addWidget(pTranslateBtn);
+    layout->addLayout(getResTextLayout());
+
+    this->setLayout(layout);
+
+    qDebug("CodeEnvWgt(constructor): Success");
 }
 
 CodeEnvWgt::~CodeEnvWgt() {
 
 
-    qDebug("CodeEnvWgt: success destructor");
+    qDebug("CodeEnvWgt(destructor): Success");
+}
+
+QLayout* CodeEnvWgt::getSrcTextLayout() {
+    /*
+     *
+     *
+    */
+
+    pOpenSourceBtn = new QPushButton("Открыть", this);
+    pSaveSourceBtn = new QPushButton("Сохранить", this);
+    pTxtEditSource = new QTextEdit("Исходный текст", this);
+    pTxtEditSource->setFont(QFont("Lucida Console", 12));
+    pTxtEditSource->setTabStopDistance(
+        QFontMetricsF(pTxtEditSource->font()).horizontalAdvance(' ') * 4);
+
+    QHBoxLayout *btnLayout = new QHBoxLayout();
+    btnLayout->addWidget(pOpenSourceBtn);
+    btnLayout->addWidget(pSaveSourceBtn);
+
+    QVBoxLayout *layout = new QVBoxLayout();
+    layout->addWidget(pTxtEditSource);
+    layout->addLayout(btnLayout);
+
+    return layout;
+}
+
+QLayout* CodeEnvWgt::getResTextLayout() {
+    /*
+     *
+    */
+
+    pSaveResultBtn = new QPushButton("Сохранить", this);
+    pTxtEditResult = new QTextEdit("Результат трансляции", this);
+    pTxtEditResult->setFont(QFont("Lucida Console", 12));
+    pTxtEditResult->setTabStopDistance(
+        QFontMetricsF(pTxtEditSource->font()).horizontalAdvance(' ') * 4);
+
+    QVBoxLayout *layout = new QVBoxLayout();
+    layout->addWidget(pTxtEditResult);
+    layout->addWidget(pSaveResultBtn);
+
+    return layout;
+
+}
+
+
+/* SLOTS */
+void CodeEnvWgt::translate() {
+    /* Взять текст из source - прогнать через процесс translator.exe
+    *   -> Вставить результать стандартонго вывода процесса в Result
+    */
+
+    qDebug() << "codeEnvWgt(translate): success translate";
 }
--- src/headers/codeenvwgt.h
+++ src/headers/codeenvwgt.h
@@ -2,13 +2,29 @@
 #define CODEENVWGT_H
 
 #include <QWidget>
+#include <QtWidgets>
 
 class CodeEnvWgt : public QWidget
 {
     Q_OBJECT
+
+private:
+    QTextEdit *pTxtEditSource = nullptr;
+    QTextEdit *pTxtEditResult = nullptr;
+    QPushButton *pTranslateBtn = nullptr;
+    QPushButton *pOpenSourceBtn = nullptr;
+    QPushButton *pSaveSourceBtn = nullptr;
+    QPushButton *pSaveResultBtn = nullptr;
+
+    QLayout* getSrcTextLayout();
+    QLayout* getResTextLayout();
 public:
     explicit CodeEnvWgt(QWidget *parent = nullptr);
     ~CodeEnvWgt();
+
+private slots:
+    void translate();
+
 signals:
 
 };
